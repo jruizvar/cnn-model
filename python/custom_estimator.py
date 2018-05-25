c1a073788ec734ccc6f6b435638b67e38dd8de2c
Jose Cupertino Ruiz Vargas <jruizvar@ncc.unesp.br>
2018-05-24 22:42:31 -0300

    Update estimator

--- python/custom_estimator.py
+++ python/custom_estimator.py
@@ -24,46 +24,38 @@ FLAGS = flags.FLAGS
 
 
 def model_fn(features, labels, mode):
-    """ Model function for CNN
+    """ Model function
     """
     input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
-
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
         filters=32,
         kernel_size=5,
         padding='same',
         activation=tf.nn.relu)
-
     pool1 = tf.layers.max_pooling2d(
         inputs=conv1,
         pool_size=2,
         strides=2)
-
     conv2 = tf.layers.conv2d(
         inputs=pool1,
         filters=64,
         kernel_size=5,
         padding='same',
         activation=tf.nn.relu)
-
     pool2 = tf.layers.max_pooling2d(
         inputs=conv2,
         pool_size=2,
         strides=2)
-
     pool2_flat = tf.reshape(pool2, [-1, 7*7*64])
-
     dense = tf.layers.dense(
         inputs=pool2_flat,
         units=1024,
         activation=tf.nn.relu)
-
     dropout = tf.layers.dropout(
         inputs=dense,
         rate=0.4,
         training=mode == tf.estimator.ModeKeys.TRAIN)
-
     logits = tf.layers.dense(
         inputs=dropout,
         units=3)
@@ -96,7 +88,7 @@ def model_fn(features, labels, mode):
 
 
 def main(_):
-    """ Load dataset dataset.
+    """ Load dataset.
         Execute custom estimator.
     """
     dataset = load_dataset()
