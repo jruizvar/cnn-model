34f0397564bd585b583c7f5b0375ed1fc85f7b84
Jose Cupertino Ruiz Vargas <jruizvar@ncc.unesp.br>
2018-05-24 23:27:57 -0300

    Explicit parameter names

--- python/custom_estimator.py
+++ python/custom_estimator.py
@@ -27,25 +27,36 @@ def dnn(inputs, mode):
     """ Design deep neural network
     """
     conv1 = tf.layers.conv2d(
-        inputs, 32, 5,
+        inputs,
+        filters=32,
+        kernel_size=5,
         padding='same',
         activation=tf.nn.relu)
     pool1 = tf.layers.max_pooling2d(
-        conv1, 2, 2)
+        conv1,
+        pool_size=2,
+        strides=2)
     conv2 = tf.layers.conv2d(
-        pool1, 64, 5,
+        pool1,
+        filters=64,
+        kernel_size=5,
         padding='same',
         activation=tf.nn.relu)
     pool2 = tf.layers.max_pooling2d(
-        conv2, 2, 2)
+        conv2,
+        pool_size=2,
+        strides=2)
     dense = tf.layers.dense(
         tf.reshape(pool2, [-1, 7*7*64]),
-        1024, activation=tf.nn.relu)
+        units=1024,
+        activation=tf.nn.relu)
     dropout = tf.layers.dropout(
         dense, rate=0.4,
         training=mode == tf.estimator.ModeKeys.TRAIN)
     logits = tf.layers.dense(
-        dropout, 3)
+        dropout,
+        units=3,
+        activation=None)
     return logits
 
 
